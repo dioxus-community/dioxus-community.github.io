40387c48c3fd3ffad45ecc616fc58444d6e9de82
Leonard <tigerros.gh@gmail.com>
2023-10-13 21:23:46 +0200

    Add active link class

--- src/components/navigation.rs
+++ src/components/navigation.rs
@@ -2,6 +2,7 @@ use dioxus::prelude::*;
 use dioxus_router::prelude::*;
 use crate::Route;
 
+const NAVBAR_LINK_ACTIVE_STYLE: &str = "bg-blue-2";
 const NAVBAR_LINK_STYLE: &str = "p-1 px-4 justify-center grow flex text-white hover:border-transparent hover:bg-blue-2 border-2 border-blue-2 rounded-md md:rounded-full transition ease-in-out delay-30";
 
 #[allow(non_snake_case)]
@@ -9,9 +10,9 @@ const NAVBAR_LINK_STYLE: &str = "p-1 px-4 justify-center grow flex text-white ho
 pub fn Navigation(cx: Scope) -> Element {
     render! {
         nav { class: "mx-auto p-2 flex flex-wrap bg-blue-1 rounded-md md:rounded-full gap-2",
-            Link { class: NAVBAR_LINK_STYLE, to: Route::Home {}, "Home" }
+            Link { active_class: NAVBAR_LINK_ACTIVE_STYLE, class: NAVBAR_LINK_STYLE, to: Route::Home {}, "Home" }
             Link { class: NAVBAR_LINK_STYLE, to: "https://github.com/dioxus-community", "GitHub" }
-            Link { class: NAVBAR_LINK_STYLE, to: Route::OurProjects {}, "Our projects" }
+            Link { active_class: NAVBAR_LINK_ACTIVE_STYLE, class: NAVBAR_LINK_STYLE, to: Route::OurProjects {}, "Our projects" }
         }
     }
 }
