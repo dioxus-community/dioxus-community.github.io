ef8edc19ed7fa54e095befd87411515957701fa7
Leonard <tigerros.gh@gmail.com>
2023-10-13 21:02:42 +0200

    Add instructions how to add your project to the organization in the "our project" page

--- src/routes/our_projects.rs
+++ src/routes/our_projects.rs
@@ -1,4 +1,5 @@
 use dioxus::prelude::*;
+use dioxus_router::prelude::*;
 use crate::OUR_PROJECTS;
 use crate::components::ProjectGrid;
 
@@ -8,7 +9,12 @@ pub fn OurProjects(cx: Scope) -> Element {
     render! {
         div { class: "text-white",
             p { class: "w-2/3 text-center mx-auto pb-4",
-                "A list of all projects that gather under the name of Dioxus Community!"
+                "A list of all our projects. If you want to add your project to our organization, let us know at the Dioxus "
+                Link { to: "https://discord.com/invite/XgGxMSkvUM", "Discord server" }
+                "! Make sure you select \"Dioxus Ecosystem\" in the \"Channels & Roles\" to see our channel.
+                Here's the "
+                Link { class: "underline", to: "https://discord.com/channels/899851952891002890/1155572382157783141", "link" }
+                "."
             }
             div { class: "mx-auto lg:w-9/12", ProjectGrid { projects: &OUR_PROJECTS } }
         }
